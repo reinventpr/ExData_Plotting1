5a2dbc2c6984fbf59a40aeefbe87ce088db3b5d5
Peifong Ren <peifongren@gmail.com>
2019-05-08 21:31:41 -0400

    Construct Plot #1

---
+++ ConstructPlot-1.R
@@ -0,0 +1,25 @@
+##
+##  Loading the data 
+##    This assignment uses data from the UC Irvine Machine Learning Repository,
+##    Dataset: Electric power consumption  
+##
+##  
+##  Making Plots 
+##    goal is to examine how household energy usage varies over a 2-day period in February, 2007
+##    reconstruct the plots like the plots in the assignment
+##
+##
+setwd("~/Downloads/")
+householdFile <- "household_power_consumption.txt"
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+
+##
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
